f067395a90bdb21ea4cd85d1db1bb0f2d7df5bcb
Daniel ikechukwu <danielikechukwu495@gmail.com>
2022-12-06 15:19:20 +0000

    Add project files.

---
+++ EFWebAPI/Data/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFWebAPI.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+
+
+    }
+}
---
+++ EFWebAPI/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace EFWebAPI.Model
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public double Price { get; set; }        
+        public string Description { get; set; }
+
+
+    }
+}
