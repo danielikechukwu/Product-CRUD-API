a6c8d2631500dd8f830f944a0b1c26bc41112bd2
Daniel ikechukwu <danielikechukwu495@gmail.com>
2022-12-16 03:11:06 +0000

    Updated feature

---
+++ EFWebMVC/Controllers/MVCProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFWebMVC.Controllers
+{
+    public class MVCProductController : Controller
+    {
+        public IActionResult MVCHome()
+        {
+            return View();
+        }
+
+        public IActionResult ShowProduct()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+    }
+}
